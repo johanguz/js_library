0b6f5768cd828611f12d415841b1bf5915ecdeb7
Morgan Edwards <medwardsii@gmail.com>
2021-02-15 17:04:50 -0500

    cleaned code

--- app.js
+++ app.js
@@ -1,50 +1,30 @@
 let myLibrary = [];
-const booksContent = document.querySelector('.myBooks');
-const formSubmitButton = document.querySelector('.submitButton')
-
-let title;
-let author;
-let read;
-let url;
-let pages;
-
-function testSubmit (e) {
-    e.preventDefault();
-    console.log(e);
-}
-
-formSubmitButton.addEventListener("click", submitBook);
+document
+    .querySelector('.submitButton')
+    .addEventListener("click", submitBook);
 
 function submitBook(e) {
     e.preventDefault();
-    let formTitle = document.getElementById('title').value;
-    let formAuthor = document.getElementById('author').value;
-    let formPages = document.getElementById('pages').value;
-    let formRead = document.getElementById('read').value;
-    let formCoverImage = document.getElementById('coverImageUrl').value;
-    const book = new Book(formTitle, formAuthor, formPages, formRead, formCoverImage);
-    console.log(book);
+    const book = getBook();
+    addBookToLibrary(book);
     addBookToPage(book);
-    resetInput();
 }
 
-function paintOnPage() {
-    console.log(myLibrary[myLibrary.length-1].author)
-    addBookToPage(myLibrary[myLibrary.length-1])
-}
-
-function resetInput() {
+function getBook() {
     let formTitle = document.getElementById('title');
     let formAuthor = document.getElementById('author');
     let formPages = document.getElementById('pages');
     let formRead = document.getElementById('read');
     let formCoverImage = document.getElementById('coverImageUrl');
+    const book = new Book(formTitle.value, formAuthor.value, formPages.value, formRead.value, formCoverImage.value);
     formTitle.value = "";
     formAuthor.value = "";
     formPages.value = "";
     formRead.value = "";
     formCoverImage.value = "";
+    return book;
 }
+
 
 function Book(title, author, pages, read, coverImageUrl) {
     this.title = title;
@@ -54,19 +34,13 @@ function Book(title, author, pages, read, coverImageUrl) {
     this.coverImageUrl = coverImageUrl;
 }
 
-Book.prototype.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, is ${this.read} `
-}
-
 function addBookToLibrary(book) {
-    myLibrary.push({title: this.title, author: this.author, pages: this.pages, read: this.read});
+    myLibrary.push({title: book.title, author: book.author, pages: book.pages, read: book.read});
 }
 
-Book.prototype.addBookToLibrary = addBookToLibrary;
-Book.prototype.addBookToPage = addBookToPage;
 
 function addBookToPage(book) {
-    console.log(book.author);
+    const booksContent = document.querySelector('.myBooks');
     booksContent.innerHTML += `
     <div class="card" style="width: 18rem;">
             <img src="${book.coverImageUrl}" class="card-img-top" alt="${book.title}">
