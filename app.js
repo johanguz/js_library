bb6e3770c187fae0cb01f2ab3129e87e46c8bbc4
johan.guzman <johanguz@gmail.com>
2021-02-16 22:00:32 -0500

    implemented a remove book function

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let myLibrary = [];
 document
     .querySelector('.submitButton')
     .addEventListener("click", submitBook);
-
+  
 function submitBook(e) {
     e.preventDefault();
     const book = getBook();
@@ -38,17 +38,34 @@ function addBookToLibrary(book) {
     myLibrary.push({title: book.title, author: book.author, pages: book.pages, read: book.read});
 }
 
+function removeBook(e) {
+    console.log(idx)
+    const bookToRemove = document.querySelector(`.idx${idx}`);
+    console.log(bookToRemove);
+    bookToRemove.remove();
+}
+
+function lookUpBook(book){
+    return myLibrary.findIndex(x => x.title === book.title);
+}
 
 function addBookToPage(book) {
     const booksContent = document.querySelector('.myBooks');
+    const idx = lookUpBook(book);
     booksContent.innerHTML += `
-    <div class="card" style="width: 18rem;">
+    <div data-bookid="${idx}" class="card idx${idx}" style="width: 18rem;">
             <img src="${book.coverImageUrl}" class="card-img-top" alt="${book.title}">
             <div class="card-body">
             <h5 class="card-title">${book.title}</h5>
             <p class="card-text">by ${book.author}, ${book.pages} pages, is ${book.read}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary removeBtn">Remove Book</a>
             </div>
         </div>`
+    document
+        .querySelector('.removeBtn')
+        .addEventListener("click", () => {
+          document.querySelector(`.idx${idx}`).remove();
+          myLibrary.splice(idx, 1);
+        })
 }
    
